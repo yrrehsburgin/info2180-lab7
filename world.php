b7a31ae98f659f824658c45f5a8d11726f5efc10
Sherricea Burgin <yrrehsburgin@gmail.com>
2018-11-19 05:13:50 +0000

    Initial Commit

---
+++ world.php
@@ -0,0 +1,13 @@
+<?php
+$host = getenv('IP');
+$username = getenv('C9_USER');
+$password = '';
+$dbname = 'world';
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+$stmt = $conn->query("SELECT * FROM countries");
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+echo '<ul>';
+foreach ($results as $row) {
+  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+}
+echo '</ul>';
