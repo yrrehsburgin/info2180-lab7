5c3c6c58eb633fc638504398ceb0fc3a5268247d
Sherricea Burgin <yrrehsburgin@gmail.com>
2018-11-20 01:19:37 +0000

    EXERCISE 4 COMPLETED

--- world.js
+++ world.js
@@ -7,25 +7,44 @@ window.onload = function() {
     
     var search = document.getElementById("country");
     
+    var all = document.getElementById("all");
+    
     var HTTPRequest;
     
 	request.addEventListener("click", function(e) {
 	    e.preventDefault();
 	    var country = search.value;
 	    
+	    if(all.checked){
+	    	HTTPRequest = new XMLHttpRequest();
+	    	HTTPRequest.onreadystatechange = getInformation;
+	    	
+	    	var url = "world.php?country=";
+	    	
+	    	HTTPRequest.open("GET", url,true);
+	    	HTTPRequest.send();
+	    	
+	    }
+	    else{
+	    
 		if(country != ""){
 			HTTPRequest = new XMLHttpRequest();
+			
 			HTTPRequest.onreadystatechange = getInformation;
-			var url = "world.php?country=" + country;
+			
+			var newurl = "world.php?country=" + country;
 	    
-	    	HTTPRequest.open("GET", url);
+	        
+	    	HTTPRequest.open("GET", newurl,true);
 	    	HTTPRequest.send();
 		}
 		
 		else{
 			result.innerHTML = "<h2>Search Result</h2><p>Please enter a country<p>";
 		}
+	  }
 	});
+	
 	
 	function getInformation() {
 	    if (HTTPRequest.readyState === XMLHttpRequest.DONE){
